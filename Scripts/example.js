a3816d59d30c59ee062fd7d439aa746c7be5ae8c
范晓豪 <fanxiaohaohaha@qq.com>
2023-05-12 08:53:01 +0800

    fix

--- Scripts/example.js
+++ Scripts/example.js
@@ -1,18 +1,18 @@
-const puppeteer = require('puppeteer');
-const path = require('path');
+const puppeteer = require('puppeteer')
+const path = require('path')
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  
-  const dir = path.join(__dirname, '..', 'screenshot'); // 将 screenshot 目录放在同级目录下
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://example.com')
+
+  const dir = path.join(__dirname, '..', 'screenshot') // 将 screenshot 目录放在同级目录下
   if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+    fs.mkdirSync(dir)
   }
-  
-  await page.screenshot({ path: path.join(dir, 'example.png') });
-  
-  await browser.close();
-})();
+
+  await page.screenshot({ path: path.join(dir, 'example.png') })
+
+  await browser.close()
+})()
